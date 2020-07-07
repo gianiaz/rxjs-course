9243349fec44e75356462ab52d0615fdbca66c30
Giovanni Lenoci <giovanni.lenoci@facile.it>
2020-07-07 17:30:18 +0200

    Hot observable

--- src/code.ts
+++ src/code.ts
@@ -1,6 +1,33 @@
 import { Observable } from "rxjs/Observable";
-import { observable } from "rxjs";
+import "rxjs/add/operator/share";
 
-var obs = Observable.create((observer: any) => observer.next("WoW"));
+var obs = Observable.create((observer: any) => {
+  try {
+    observer.next("WoW");
+    observer.next("how are you?");
+    setInterval(() => {
+      observer.next("I am good");
+    }, 2000);
+    //observer.complete();
+    observer.next("this will not send");
+  } catch (err) {
+    observer.error(err);
+  }
+}).share();
 
-obs.subscribe((x: any) => console.log(x));
+var observer = obs.subscribe(
+  (x: any) => addItem(x),
+  (error: any) => addItem(error),
+  () => addItem("completed")
+);
+
+setTimeout(() => {
+  var observer2 = obs.subscribe((x: any) => addItem("Subscriber 2: " + x));
+}, 1000);
+
+function addItem(val: any) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("output").appendChild(node);
+}
